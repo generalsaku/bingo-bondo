24582816ad309d0f7c06213e50415e1f636c22b6
generalsaku <josefgdev@gmail.com>
2022-06-06 20:40:01 +0200

    font changes

--- src/App.vue
+++ src/App.vue
@@ -76,7 +76,7 @@ const createBingoOptions = () => shuffle(options).slice(0, 25).map((option) => (
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500;600,700&display=swap');
 
 body,html {
   margin: 0;
@@ -105,10 +105,12 @@ table {
     padding: 0;
     outline: 1px dashed #c7c7c7;
     cursor: pointer;
+    font-size: 85%;
 
     &.is-active {
       background: #a3f1a3;
       outline: 1px dashed #00bf00;
+      font-weight: 600;
     }
   }
 }
